7a5e2fc333f0bb5e8a593e548ef203e38450b35b
Cristian-Mathias <cristian11mathias@hotmail.com>
2025-12-28 18:31:19 -0300

    chore(security): desabilitar autenticação padrão do Spring Security

--- src/main/java/br/com/challenge/forumhub/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/challenge/forumhub/infra/security/SecurityConfigurations.java
@@ -1,18 +1,26 @@
 package br.com.challenge.forumhub.infra.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
-        httpSecurity
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-        return httpSecurity.build();
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement( session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+        return http.build();
     }
 }
