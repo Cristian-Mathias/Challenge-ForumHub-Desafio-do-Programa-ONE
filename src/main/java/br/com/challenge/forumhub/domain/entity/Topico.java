eeab67f8b13683da2dce0a5a93bb05cfb0f8c561
Cristian-Mathias <cristian11mathias@hotmail.com>
2025-12-29 12:13:13 -0300

    refactor(entity): ajustar entidade Topico

--- src/main/java/br/com/challenge/forumhub/domain/entity/Topico.java
+++ src/main/java/br/com/challenge/forumhub/domain/entity/Topico.java
@@ -1,14 +1,11 @@
-package br.com.challenge.forumhub.domain.topico;
+package br.com.challenge.forumhub.domain.entity;
 
+import br.com.challenge.forumhub.domain.enums.EstadoTopico;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "topicos")
@@ -24,9 +21,62 @@ public class Topico {
     @Column(name = "data_criacao", nullable = false)
     private LocalDateTime dataCriacao;
     @Column(name = "estado_topico", nullable = false)
-    private String estado;
+    @Enumerated(EnumType.STRING)
+    private EstadoTopico estado;
     private String autor;
     private String curso;
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public EstadoTopico getEstado() {
+        return estado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public void setEstado(EstadoTopico estado) {
+        this.estado = estado;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
     @PrePersist
     public void prePersist(){
