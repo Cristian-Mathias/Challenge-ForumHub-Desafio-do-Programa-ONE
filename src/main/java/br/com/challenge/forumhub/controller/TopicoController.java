f12f3703fbc825f5b8f2beae8e0a715d9c5dae9d
Cristian-Mathias <cristian11mathias@hotmail.com>
2025-12-29 15:01:21 -0300

    fix: incluir path da requisição na resposta de erro do tópico

--- src/main/java/br/com/challenge/forumhub/controller/TopicoController.java
+++ src/main/java/br/com/challenge/forumhub/controller/TopicoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/topicos")
+@RequestMapping(path ="/topicos")
 public class TopicoController {
 
     private final TopicoService service;
