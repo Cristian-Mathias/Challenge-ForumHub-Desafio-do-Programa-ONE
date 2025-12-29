83c3dfc84c3aa714284cbd9e72b5ffc184b0d5e0
Cristian-Mathias <cristian11mathias@hotmail.com>
2025-12-29 14:55:48 -0300

    feat: adicionar método para verificar tópico duplicado

--- src/main/java/br/com/challenge/forumhub/domain/repository/TopicoRepository.java
+++ src/main/java/br/com/challenge/forumhub/domain/repository/TopicoRepository.java
@@ -4,4 +4,5 @@ import br.com.challenge.forumhub.domain.entity.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    boolean existsByTituloAndMensagem(String titulo, String mensagem);
 }
